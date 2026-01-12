702fe0337611ce86fb01c73c602c9639106e5831
momoyelin46 <momoyelin46@gmail.com>
2026-01-12 19:49:13 +0200

    enable CORS for client access

--- backend/TicketApi/TicketApi/Program.cs
+++ backend/TicketApi/TicketApi/Program.cs
@@ -7,6 +7,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("ClientCors", policy =>
+    {
+        policy
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,6 +29,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("ClientCors");
 
 app.UseAuthorization();
 
